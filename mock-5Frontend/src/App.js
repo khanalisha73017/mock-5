a5501b6e7de1e1eea3cf1b97a97c92034b33d834
khanalisha73017 <152811469+khanalisha73017@users.noreply.github.com>
2023-12-07 20:31:20 +0530

    Add files via upload

---
+++ mock-5Frontend/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+import { NavBar } from "./page/NavBar";
+import { AllRoutes } from "./page/AllRoutes";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <AllRoutes />
+    </>
+  );
+}
+
+export default App;
---
+++ mock-5Frontend/src/component/SignUp.jsx
@@ -0,0 +1,72 @@
+import { Button, VStack } from "@chakra-ui/react";
+import React, { useState } from "react";
+import axios from "axios";
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Input,
+} from "@chakra-ui/react";
+import { useDispatch } from "react-redux";
+import { registerUser } from "../redux/userReducer/action";
+
+export const SignUp = () => {
+  const [username, setusername] = useState("");
+  const [avatar, setAvatar] = useState("");
+  const [email, setemail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const dispatch = useDispatch();
+
+  const handleSubmit = () => {
+    const obj = {
+      username,
+      avatar,
+      email,
+      password,
+    };
+    dispatch(registerUser(obj));
+  };
+
+  return (
+    <VStack>
+      <FormControl>
+        <FormLabel>User Namme</FormLabel>
+        <Input
+          type="text"
+          value={username}
+          onChange={(e) => setusername(e.target.value)}
+        />
+        <br />
+        <br />
+        <FormLabel>Avater</FormLabel>
+        <Input
+          type="text"
+          value={avatar}
+          onChange={(e) => setAvatar(e.target.value)}
+        />
+        <br />
+        <br />
+        <FormLabel>Email address</FormLabel>
+        <Input
+          type="email"
+          value={email}
+          onChange={(e) => setemail(e.target.value)}
+        />
+        <br />
+
+        <FormLabel>Password</FormLabel>
+        <Input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <br />
+
+        <Button onClick={handleSubmit}>Register</Button>
+      </FormControl>
+    </VStack>
+  );
+};
